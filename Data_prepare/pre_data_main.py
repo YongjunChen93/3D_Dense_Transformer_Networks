8bd97673f947a5e1030f68d2b13d295df2727857
JohnYC1995 <Juhn@Yongjuns-MBP.netgear.com>
2017-06-15 07:42:39 -0700

    add save all dataset name to txt function
    add save all dataset name to txt function

--- Data_prepare/pre_data_main.py
+++ Data_prepare/pre_data_main.py
@@ -1,7 +1,7 @@
 #python 3.0
 #coding:UTF-8
 '''
-@author Yongjun Chen 06/01/2017
+@author Yongjun Chen 06/05/2017
 '''
 #for  set up the dataset
 import h5py
@@ -13,14 +13,14 @@ from prepare_data import *
 
 def main():
 	dataFile =      'LONI_valid.h5'
-	dataFileresult = 'LONI_v.h5'
+	dataFileresult = 'LONI_train.h5'
 	print("dataFile1",dataFile)
 	size = (10,181,142,149)
 	check_name(dataFile)
 	data,label = read_data(dataFile,size,Transpose=False,resize=False)
 	save(data,label,dataFileresult)
+	make_data_loc_file('../augment_data/LONI_train','../augment_data/LONI_train/train.txt')
 	show_data(data,label)
 
 if __name__ == '__main__':
 	main()
-
