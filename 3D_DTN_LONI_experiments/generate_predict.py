e30362d2e8c91bbc517ee43a749911bff9281f74
JohnYC1995 <Juhn@Yongjuns-MacBook-Pro.local>
2017-07-15 22:24:43 -0700

    update generate predict_func
    update generate predict_func

--- 3D_DTN_LONI_experiments/generate_predict.py
+++ 3D_DTN_LONI_experiments/generate_predict.py
@@ -1,4 +1,9 @@
 import os
+import re
+
+file_path = './main.py'
 for i in range(2,21):
-    print("generate predict file",i)
-    os.system("cp main.py main"+str(i)+".py")
+	f = open(file_path,"r+")
+	print("generate_file:",i)
+	open('main'+str(i)+'.py', 'w').write(re.sub('index = 1', 'index = '+str(i), f.read()))
+	f.close()
